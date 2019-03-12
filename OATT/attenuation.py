32891caaa3268bf1e1a655a826523fad449ce191
Schweif <david.just@psi.ch>
2019-03-12 16:05:16 +0100

    added thin Al foils for Kirsten

--- OATT/attenuation.py
+++ OATT/attenuation.py
@@ -8,7 +8,7 @@ from os.path import isfile, join
 import matplotlib.pyplot as plt
 from itertools import combinations
 
-pathToAttenuatorFoils = '/home/just/Documents/PSI/OATT/materials/CandSiOnly/selection20'
+pathToAttenuatorFoils = '/home/just/Documents/PSI/OATT/materials/selection18Kirsten/'
 
 
 # append the rawData from all Foils
