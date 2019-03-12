c49086161576a21050f4941201e379213b907ad8
Schweif <david.just@psi.ch>
2019-03-12 14:46:01 +0100

    added selection11

--- OATT/attenuation.py
+++ OATT/attenuation.py
@@ -8,7 +8,7 @@ from os.path import isfile, join
 import matplotlib.pyplot as plt
 from itertools import combinations
 
-pathToAttenuatorFoils = '/home/just/Documents/PSI/OATT/materials/'
+pathToAttenuatorFoils = '/home/just/Documents/PSI/OATT/materials/selection11/'
 
 
 # append the rawData from all Foils
