b11ccd9dce034f43882c35120e46f025d3f82474
Schweif <david.just@psi.ch>
2019-11-04 17:05:23 +0100

    Fixed bug when calculating Sigma from fit

--- calculate3Sigma.py
+++ calculate3Sigma.py
@@ -3,6 +3,7 @@
 #imports for 3D Ploting of collected data
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.mlab as ml
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
@@ -14,80 +15,11 @@ import pylab as plb
 from scipy.optimize import curve_fit
 from scipy import asarray as ar,exp
 
-csv=True
+csv=False
 
-DataRaw =  '/home/just/Documents/PSI/XBPM/rawData/ComparisonSLS_SLS2.0/Dtl_SLS1_U14_K165_10_30000eV/SLS_U14_K165_30_30000eV_detailed_CRXO.csv'
+DataRaw =  '/home/just/Documents/PSI/rawData/06_U14_SLS1_K1.5_PowerDensity_10m.dta'
 dist_from_source = 10 #m
-import numpy as np #did not work on first imort doing it twice instead
 
-if csv== True:
-    da= np.genfromtxt(DataRaw,delimiter=",",skip_header=1, usecols=(1, 2, 3))
-else:
-    da= np.genfromtxt(DataRaw,skip_header=10, usecols=(0, 1, 2))
-
-xdata= da[:,0]
-ydata= da[:,1]
-zdata= da[:,2]
-
-
-
-ax = plt.axes(projection='3d')
-ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
-plt.show()
-
-
-
-
-'''
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-'''
-
-
-#cut throu x=0
-x0zdata=[]
-x0ydata=[]
-
-for d in da:
-    if d[0] == 0:
-        x0ydata.append(d[1])
-        x0zdata.append(d[2])
-
-#cut throu y=0
-y0zdata=[]
-y0xdata=[]
-
-for d in da:
-    if d[1] == 0:
-        y0xdata.append(d[0])
-        y0zdata.append(d[2])
-
-
-
-#get Numpy arrays from list
-x0ydata=np.array(x0ydata)
-x0zdata=np.array(x0zdata)
-y0xdata=np.array(y0xdata)
-y0zdata=np.array(y0zdata)
-
-# for x=0
-nY = len(x0zdata)
-meanY = sum(x0ydata*x0zdata)/ sum(x0zdata)
-sigmaY = np.sqrt(sum(x0zdata*(x0ydata-meanY)**2)/sum(x0zdata))
-
-# for y=0
-nX = len(y0zdata)
-meanX = sum(y0xdata*y0zdata)/ sum(y0zdata)
-sigmaX = np.sqrt(sum(y0zdata*(y0xdata-meanX)**2)/sum(y0zdata))
 
 def Gauss(x,a,x0,sigma):
     return a*exp(-(x-x0)**2/(2*sigma**2))
@@ -109,19 +41,25 @@ def FitAndPlot2DGauss(axis):
         n= len(y)
         mean = sum(x * y) / sum(y)
         sigma = np.sqrt(sum(y * (x - mean)**2) / sum(y))
+
+ 
+    popt,pcov = curve_fit(Gauss, x, y, p0=[max(y), mean, sigma])
+    ampl  = popt[0]
+    mean  = popt[1]
+    sigmaN = popt[2]
+
     print "*********************************"
     print "* Results for "+axis +" - axis          *"
     print "* (x = horizontal, y = vertical)*"
     print "*********************************"
-    print "sigma = " +str(sigma) +" mm"
-    print "sigma = " +str(sigma/dist_from_source) +" mrad"
-    print "3 sigma = " +str(3*sigma) +" mm"
-    print "3 sigma = " +str(3*sigma/dist_from_source) +" mrad"
+    print "sigma (est)= " +str(sigma) +" mm"
+    print "sigma (fit)= " +str(sigmaN) +" mm"
+    print "sigma = " +str(sigmaN/dist_from_source) +" mrad"
+    print "3 sigma = " +str(3*sigmaN) +" mm"
+    print "3 sigma = " +str(3*sigmaN/dist_from_source) +" mrad"
     print "mean = " +str(mean)
     print "************************************************"
- 
-    popt,pcov = curve_fit(Gauss, x, y, p0=[max(y), mean, sigma])
-    #print "popt = " +str(popt)
+
     #print "pcov = " +str(pcov)
     plt.plot(x, y, 'b+:', label='data')
     plt.plot(x, Gauss(x, *popt), 'r-', label='fit')
@@ -129,48 +67,108 @@ def FitAndPlot2DGauss(axis):
     plt.title(axis+' Axis')
     plt.xlabel('length (mm)')
     plt.ylabel('PowerDens (W/mm**2)')
-    plt.text(1,0,"sigma = " +str(round(sigma,3)) +" mm", fontsize=12, horizontalalignment='left', verticalalignment='bottom')
-    plt.show()    
+    plt.text(1,0,"sigma = " +str(round(sigmaN,3)) +" mm", fontsize=12, horizontalalignment='left', verticalalignment='bottom')
+    plt.show()  
 
+  
+def plot2D(x, y, z,txt=''):
+    if not 'title' in globals():
+        title = ''
+    if not 'autoSave' in globals():
+        autoSave = False
+    fname = title + '_2D' + txt + '.png' 
+    xmax= np.max(x)
+    ymax= np.max(y)
+    xmin= np.min(x)
+    ymin= np.min(y)
+    nx = len(x)
+    ny= len(y)
     
+    #xi = np.linspace(-5, 5, nx)
+    #yi = np.linspace(-5, 5, ny)
+    xi = np.linspace(xmin, xmax, nx)
+    yi = np.linspace(ymin, ymax, ny)
+    zi = ml.griddata(x, y, z, xi, yi)
+    CS = plt.contourf(xi, yi, zi, 15, colors = 'k')
+    CS2 = plt.pcolormesh(xi, yi, zi, cmap = plt.get_cmap('rainbow'))
+    plt.title(title)
+    plt.xlabel('x, position hor. [mm]')
+    plt.ylabel('y, position ver. [mm]')
+
+    cbar = plt.colorbar(CS2) 
+    cbar.ax.set_ylabel("Flux, (arbitary)")
+    
+    #plt.scatter(x, y, marker = 'o', c = 'b', s = 5, zorder = 10)
+    #plt.scatter(x, y, c = 'b', s = 5, zorder = 10)
+    plt.xlim(-7, 7)
+    plt.ylim(-7, 7)
+    #plt.xlim(xmin, xmax)
+    #plt.ylim(ymin, ymax)
+    if autoSave == True:
+        plt.savefig(fname)
+        plt.close()
+        plt.clf()
+    else:
+        plt.show()
+        plt.clf()
+    #  see: https://stackoverflow.com/questions/13781025/matplotlib-contour-from-xyz-data-griddata-invalid-index
+
+##Prepare Data
+if csv== True:
+    da= np.genfromtxt(DataRaw,delimiter=",",skip_header=1, usecols=(1, 2, 3))
+else:
+    da= np.genfromtxt(DataRaw,skip_header=10, usecols=(0, 1, 2))
+
+xdata= da[:,0]
+ydata= da[:,1]
+zdata= da[:,2]
+
+
+#cut throu x=0
+x0zdata=[]
+x0ydata=[]
+
+for d in da:
+    if d[0] == 0:
+        x0ydata.append(d[1])
+        x0zdata.append(d[2])
+
+#cut throu y=0
+y0zdata=[]
+y0xdata=[]
+
+for d in da:
+    if d[1] == 0:
+        y0xdata.append(d[0])
+        y0zdata.append(d[2])
+
+#get Numpy arrays from list
+x0ydata=np.array(x0ydata)
+x0zdata=np.array(x0zdata)
+y0xdata=np.array(y0xdata)
+y0zdata=np.array(y0zdata)
+
+##Estimate starting vallues for fit
+# for x=0
+nY = len(x0zdata)
+meanY = sum(x0ydata*x0zdata)/ sum(x0zdata)
+sigmaY = np.sqrt(sum(x0zdata*(x0ydata-meanY)**2)/sum(x0zdata))
+
+# for y=0
+nX = len(y0zdata)
+meanX = sum(y0xdata*y0zdata)/ sum(y0zdata)
+sigmaX = np.sqrt(sum(y0zdata*(y0xdata-meanX)**2)/sum(y0zdata))
+
+
+##Plot and Print
+#3D Plot
+ax = plt.axes(projection='3d')
+ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+plt.show()
+
+#2D Colour Plot
+plot2D(xdata, ydata, zdata)
+
+#Cut Plots    
 FitAndPlot2DGauss('x')
 FitAndPlot2DGauss('y')
-
-
-
-
-'''
-
-plt.plot(x0ydata,x0zdata,'b+:',label='data')
-plt.plot(x0ydata,Gauss(x0ydata,*popt),'ro:',label='fit')
-plt.legend()
-plt.title('X=0, Gaussfit')
-plt.xlabel('Y in mm')
-plt.ylabel('Flux Desity')
-plt.show()
-'''
-'''
-check:https://stackoverflow.com/questions/19206332/gaussian-fit-for-python
-import numpy as np
-import matplotlib.pyplot as plt
-
-DataRaw =  
-testFile = open(DataRaw,'r')
-lines = testFile.readlines()
-testFile.close
-
-data = []
-i =0
-
-for line in lines[10:]:
-    data.append([line[:13], line[14:26], line[32:41]])
-data
-
-dat= dat.astype(np.float)
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-da= np.genfromtxt(DataRaw,skip_header=10, usecols=(0, 1, 2))
-da[:,0].min()
-######################'''
-
